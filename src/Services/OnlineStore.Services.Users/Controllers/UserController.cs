1fe6d71e52d28a58488d08257e546ea3bca959b3
Artem Sazonov <bennettst@yandex.ru>
2020-05-04 20:00:54 +0700

    Added ocelot and user action

---
+++ src/Services/OnlineStore.Services.Users/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStore.Services.Users.Models;
+using System.Net;
+
+namespace OnlineStore.Services.Users.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        [Route("info/{id:int}")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(UserInfo), (int)HttpStatusCode.OK)]
+        public ActionResult<UserInfo> GetInfoById(int id)
+        {
+            if (id <= 0)
+            {
+                return NotFound();
+            }
+            
+            var info = new UserInfo
+            {
+                FirstName = "Anna",
+                LastName = "Rog"
+            };
+
+            return info;
+        }
+    }
+}
---
+++ src/Services/OnlineStore.Services.Users/Models/UserInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineStore.Services.Users.Models
+{
+    public class UserInfo
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
